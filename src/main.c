afca2eec4af8af70c3f3f80517212b1199e40495
sharhar <wiishahar@gmail.com>
2017-08-07 19:17:00 -0700

    some basic code

--- src/main.c
+++ src/main.c
@@ -1,3 +1,23 @@
+#include <swin/SWin.h>
+
 int main() {
+	swInit();
+
+	SWindow* window = swCreateWindow(1000, 620, "Grapher2D");
+	SView* rootView = swGetRootView(window);
+
+	SOpenGLView* glView = swCreateOpenGLView(rootView, swMakeRect(390, 10, 600, 600));
+
+	swMakeContextCurrent(glView);
+
+	while (!swCloseRequested(window)) {
+		swPollEvents();
+
+		swSwapBufers(glView);
+		swDraw(window);
+	}
+
+	swCloseWindow(window);
+
 	return 0;
 }
