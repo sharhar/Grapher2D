947327b3e2d46a4aeb7627171035842533402324
sharhar <wiishahar@gmail.com>
2017-08-10 18:44:43 -0700

    Finished prototype UI

--- src/main.c
+++ src/main.c
@@ -1,5 +1,46 @@
 #include <swin/SWin.h>
 #include <glad/glad.h>
+#include <malloc.h>
+
+typedef struct Entry {
+	STextField* textField;
+	SButton* submitButton;
+	SButton* deleteButton;
+
+	struct Entry* next;
+} Entry;
+
+typedef struct UIState {
+	SView* rootView;
+	int y;
+	int first;
+
+	Entry* root;
+	Entry* last;
+} UIState;
+
+void submitCallback(Entry* entry) {
+	printf("%s\n", swGetTextFromTextField(entry->textField));
+}
+
+void createEntry(UIState* uiState) {
+	Entry* entry = malloc(sizeof(Entry));
+	entry->textField = swCreateTextField(uiState->rootView, swMakeRect(10, 580 - uiState->y * 40, 285, 30), "");
+	entry->submitButton = swCreateButton(uiState->rootView, swMakeRect(305, 580 - uiState->y * 40, 75, 30), "Submit", &submitCallback, entry);
+	entry->deleteButton = NULL;
+	entry->next = NULL;
+
+	if (uiState->first) {
+		uiState->root = entry;
+		uiState->last = entry;
+		uiState->first = 0;
+	} else {
+		uiState->last->next = entry;
+		uiState->last = entry;
+	}
+
+	uiState->y++;
+}
 
 int main() {
 	swInit();
@@ -7,7 +48,34 @@ int main() {
 	SWindow* window = swCreateWindow(1000, 620, "Grapher2D");
 	SView* rootView = swGetRootView(window);
 
-	SOpenGLView* glView = swCreateOpenGLView(rootView, swMakeRect(390, 10, 600, 600));
+	SOpenGLContextAttribs attribs;
+	attribs.major = 3;
+	attribs.minor = 3;
+	attribs.debug = 0;
+	attribs.swapInterval = 1;
+
+	SOpenGLView* glView = swCreateOpenGLView(rootView, swMakeRect(390, 10, 600, 600), &attribs);
+
+	UIState* state = malloc(sizeof(UIState));
+	state->y = 0;
+	state->first = 1;
+	state->rootView = rootView;
+
+	createEntry(state);
+	createEntry(state);
+	createEntry(state);
+	createEntry(state);
+	createEntry(state);
+	createEntry(state);
+	createEntry(state);
+	createEntry(state);
+	createEntry(state);
+	createEntry(state);
+	createEntry(state);
+	createEntry(state);
+	createEntry(state);
+	createEntry(state);
+	createEntry(state);
 
 	swMakeContextCurrent(glView);
 
@@ -16,10 +84,7 @@ int main() {
 		return 0;
 	}
 
-	glClearColor(1.0f, 0.0f, 1.0f, 1.0f);
-
-	//GLuint vao;
-	//glGenVertexArrays(1, &vao);
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 
 	while (!swCloseRequested(window)) {
 		swPollEvents();
